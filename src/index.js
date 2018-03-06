3a7b5c0b0b4f123126b8d84b741972d8c7f3e3c8
Christian Tschugg <c.tschugg@mmc-agentur.at>
2018-03-06 16:00:41 +0100

    Refactor loader:  - use glob for partials, data and helpers  - use lodash.merge for data merge  - add all files to watcher

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const loaderUtils = require('loader-utils');
 const fs = require('fs');
 const path = require('path');
+const glob = require('glob');
 const Handlebars = require('handlebars');
+const merge = require('lodash.merge');
+
 let initializedPartials = false;
+let initializedHelpers = false;
 let initializedData = false;
+const _data = [];
 const languages = [];
 
 function removeExtension(path) {
@@ -19,43 +24,118 @@ function getRelativePath(path) {
   return '../'.repeat(deep);
 }
 
-module.exports = function(source, map) {
-  const options = loaderUtils.getOptions(this);
-
+function loadPartials(context, options){
   if (!initializedPartials) {
-    //TODO: implement recursive readdir for nested partials support
-    //TODO: implement watch partial changes and rebuild
-    const partials = fs.readdirSync(options['partials']);
-    const partialsFolderName = options['partials'].substr(options['partials'].lastIndexOf('/') + 1);
-    const partialsPath = `${options['partials']}/`;
-    partials.forEach((partial) => {
-      //Remove *.handlebars *.hbs in key
-      const partialKey = removeExtension(partial);
-      Handlebars.registerPartial(`${partialsFolderName}/${partialKey}`, fs.readFileSync(path.join(partialsPath + partial), 'utf8'));
+    let partials;
+    if (typeof options.partials === 'string') {
+      partials = [options.partials];
+    } else {
+      partials = options.partials;
+    }
+    partials.forEach(partialGlob => {
+      glob.sync(partialGlob).forEach(partial => {
+        const partialName = options.partialNamer.call(context, partial);
+        const partialPath = path.resolve(partial);
+        context.addDependency(partialPath);
+        Handlebars.registerPartial(partialName, fs.readFileSync(partialPath, 'utf-8'));
+      });
     });
     initializedPartials = true;
   }
+}
 
-  //TODO: implement watch data changes and rebuild
+function loadHelpers(context, options) {
+  if (!initializedHelpers) {
+    let helpers;
+    if (typeof options.helpers === 'string') {
+      helpers = [options.helpers];
+    }
+    else {
+      helpers = options.helpers;
+    }
+    helpers.forEach(helperGlob => {
+      glob.sync(helperGlob).forEach(helper => {
+        const helperName = options.helperNamer.call(context, helper);
+        const helperPath = path.resolve(helper);
+        context.addDependency(helperPath);
+        delete require.cache[require.resolve(helperPath)];
+        Handlebars.registerHelper(helperName, require(helperPath))
+      })
+    });
+    initializedHelpers = true;
+  }
+}
+
+function loadData(context, options) {
   if (!initializedData) {
-    const data = fs.readdirSync(path.join(options['data']));
-    data.forEach((data) => {
-      if (data.indexOf('_') === 0) {
-        return;
-      }
-      languages.push(removeExtension(data));
+    let dataFiles;
+    if (typeof options.data === 'string') {
+      dataFiles = [options.data];
+    }
+    else {
+      dataFiles = options.data;
+    }
+    dataFiles.forEach(dataGlob => {
+      glob.sync(dataGlob).forEach(dataObj => {
+        const dataName = removeExtension(dataObj.substr(dataObj.lastIndexOf('/') + 1));
+        const dataPath = path.resolve(dataObj);
+        context.addDependency(dataPath);
+        if (dataName.indexOf('_') === 0) {
+          _data.push(JSON.parse(fs.readFileSync(dataObj)));
+          return;
+        }
+        languages.push({
+          name: dataName,
+          data: JSON.parse(fs.readFileSync(dataObj, 'utf8'))
+        });
+      })
     });
     initializedData = true;
   }
+}
+
+function defaultNamer(file) {
+  const fileStart = file.lastIndexOf('/') + 1;
+  const basePath = file.substr(0, fileStart - 1);
+  const folderStart = basePath.lastIndexOf('/') + 1;
+  const fileName = file.substr(fileStart);
+  const folderName = basePath.substr(folderStart);
+  return removeExtension(`${folderName}/${fileName}`);
+}
+
+function defaultHelperNamer(file) {
+  return defaultNamer(file).replace('.helper', '');
+}
+
+module.exports = function(source, map) {
+
+  const options = Object.assign({}, {
+    partialNamer: defaultNamer,
+    helperNamer: defaultHelperNamer
+  }, loaderUtils.getOptions(this));
+
+  if (options.partials) {
+    loadPartials(this, options);
+  }
+
+  if (options.helpers) {
+    loadHelpers(this, options);
+  }
+
+  if (options.data) {
+    loadData(this, options);
+  }
 
   languages.forEach((language)=>{
-    let languagePath = `/${language}`;
-    if (language === options['rootData']) {
+    let languagePath = `/${language.name}`;
+    if (language.name === options['rootData']) {
       languagePath = '';
     }
+
+    let data = _data.reduce((reducedData, dataObject) => merge(reducedData, dataObject), {});
     const relativePath = `${options['outputpath']}${languagePath}${removeExtension(this.resourcePath.substr(this.resourcePath.indexOf(options['relativePathTo']) + options['relativePathTo'].length))}.html`;
-    const data = JSON.parse(fs.readFileSync(path.join(options['data'], `${language}.json`), 'utf8'));
-    //TODO: implement lodash merge with _.data.json files
+    data = merge(data, language.data);
+
     data.absRefPrefix = getRelativePath(relativePath);
     const template = Handlebars.compile(source);
     const result = template(data);
