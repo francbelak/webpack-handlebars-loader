b11b4540b137bc2312fa5575c5b35b27da3f6267
Franc Belak <f.belak@mmc-agentur.at>
2018-03-08 14:29:48 +0100

    Add tests for utils

---
+++ src/utils/utils.test.js
@@ -0,0 +1,22 @@
+/* global expect */
+import { removeExtension, getRelativePath, defaultNamer, defaultHelperNamer } from './utils.js';
+
+test('Remove JS extension from file', () => {
+  expect(removeExtension('dist/js/main.js')).toEqual('dist/js/main');
+});
+
+test('Expect relative path to be in root depth', () => {
+  expect(getRelativePath('dist/index.html')).toEqual('./');
+});
+
+test('Expect relative path to be in language depth', () => {
+  expect(getRelativePath('dist/de/index.html')).toEqual('../');
+});
+
+test('Test defaultNamer to use folder and name', () => {
+  expect(defaultNamer('folder/dist/main.js')).toEqual('dist/main');
+});
+
+test('Remove helper string from name in register by default', () => {
+  expect(defaultHelperNamer('src/js/helpers/concat.helper.js')).toEqual('helpers/concat');
+});
